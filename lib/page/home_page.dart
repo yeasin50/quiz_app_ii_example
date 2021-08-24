17c953986c02dd7631aa30358379f487c24ed856
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-08-25 05:57:26 +0600

    movedAppBarOnWidget

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,39 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app_ii_example/data/categories.dart';
 import 'package:quiz_app_ii_example/data/user.dart';
 import 'package:quiz_app_ii_example/page/category_page.dart';
+import 'package:quiz_app_ii_example/widget/appBar.widget.dart';
 import 'package:quiz_app_ii_example/widget/category_detail_widget.dart';
 import 'package:quiz_app_ii_example/widget/category_header_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Text('School Quiz'),
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(80),
-            child: Container(
-              padding: EdgeInsets.all(16),
-              alignment: Alignment.centerLeft,
-              child: buildWelcome(username),
-            ),
-          ),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.deepOrange, Colors.purple],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-              ),
-            ),
-          ),
-          actions: [
-            Icon(Icons.search),
-            SizedBox(width: 12),
-          ],
-        ),
+        appBar: MyAppBar(username: username),
         body: ListView(
           physics: BouncingScrollPhysics(),
           padding: const EdgeInsets.all(16),
@@ -45,24 +20,6 @@ class HomePage extends StatelessWidget {
             buildPopular(context),
           ],
         ),
-      );
-
-  Widget buildWelcome(String username) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Hello',
-            style: TextStyle(fontSize: 16, color: Colors.white),
-          ),
-          Text(
-            username,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          )
-        ],
       );
 
   Widget buildCategories() => Container(
